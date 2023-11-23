9ce901a6315810dc02ab2741f4c67de559dec780
derodero24 <k.itsh20524abc@gmail.com>
2023-11-23 13:25:19 +0900

    update

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -2,16 +2,17 @@ import { AsyncLocalStorage } from 'async_hooks';
 import { randomUUID } from 'crypto';
 
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
+
+import type { NextFunction, Request, Response } from 'express';
 
 export const storage = new AsyncLocalStorage<string>();
 
 @Injectable()
 export class CustomLoggerMiddleware implements NestMiddleware {
-  use(req: Request, _res: Response, next: NextFunction) {
+  use(_req: Request, _res: Response, next: NextFunction) {
     // Generate unique ID
-    const requestId = randomUUID().split('-').pop();
+    const requestId = randomUUID().split('-').pop() ?? '';
     // Save to AsyncLocalStorage
-    storage.run(requestId, next, undefined);
+    storage.run(requestId, next, 'route');
   }
 }
